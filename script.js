fca90b07d3c44a53bc00453452a38d6547913f36
Mayank-Khurmaim <mayankkhurmai8@gmail.com>
2021-05-10 12:50:37 +0530

    Init

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 function pay_in(){
-    alert();
+    var x = document.getElementById("btn-primary");
+    var y = document.getElementById("btn-secondary");
+    x.style.display = "none";
+    y.style.display = "block";
 }
